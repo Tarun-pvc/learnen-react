b3ba5a9483ed7e312b1e34468c64f3142cdc401a
TEJA SANJEEV <chtsanjeev2003@gmail.com>
2023-11-03 11:45:22 +0530

    added new webpages(login,signup,otp verfication)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import AdminDashboardMain from './pages/AdminDashboard/adminDashboardMain';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignUp from './pages/SignUp/SignUp';
+import VerificationPage from './pages/VerificationPage/VerificationPage';
 
 function App() {
   return (
     <div className="App">
       <AdminDashboardMain/>
+      <LoginPage></LoginPage>
+      <SignUp></SignUp>
+      <VerificationPage></VerificationPage>
     </div>
   );
 }
