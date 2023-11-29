1990244c1ca70af63cfcfc3e8e94bd57b992dfee
TEJA SANJEEV <chtsanjeev2003@gmail.com>
2023-11-29 20:55:42 +0530

    added todo in mentorDashboard

--- client/src/pages/MentorDashboard/components/MentorDashboard.jsx
+++ client/src/pages/MentorDashboard/components/MentorDashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../MentorDashboard.css";
 import MdSideBar from "./MdSideBar";
 import MdMiddle from "./MdMiddle";
+import MDRightBar from "./MDRightBar";
 // import { useRef } from 'react';
 
 function MentorDashboard() {
@@ -9,6 +10,7 @@ function MentorDashboard() {
     <>
       <MdSideBar></MdSideBar>
       <MdMiddle></MdMiddle>
+      <MDRightBar></MDRightBar>
     </>
   );
 }
